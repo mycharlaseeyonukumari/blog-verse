79bd20979baec0f3aa802284df08c325216270c3
mycharlaseeyonukumari@gmail.com <mycharlaseeyonukumari@gmail.com>
2025-09-06 15:07:52 +0530

    learn controlflow

---
+++ controlflow.js
@@ -0,0 +1,13 @@
+let marks=85
+if(marks>=90){
+    console.log("A+ grade")
+}
+else if(marks>=80){
+    console.log("A grade")
+}
+else if(marks>=70){
+    console.log("B grade")
+}
+else{
+    console.log("fail")
+}
