5d7329b215aa10149dda08ee9a6056cebc4da9a3
mycharlaseeyonukumari@gmail.com <mycharlaseeyonukumari@gmail.com>
2025-09-06 12:38:36 +0530

    learn datatypes iin js

--- practice.js
+++ practice.js
@@ -1,1 +1,25 @@
 console.log("hello world,seeyonu kumari")
+let name = "seeyonu"
+console.log(name)
+let num=43
+console.log(num)
+let isLogged= false
+console.log(typeof isLogged)
+let x
+console.log(x)
+let y=null
+console.log(y)
+
+console.log(typeof name)
+console.log(typeof num)
+console.log(typeof x)
+console.log(typeof y)
+let arr=[1,2,3,4,5]
+console.log(arr[0])
+let person={
+    name:"seeyonu",
+    age:19
+}
+console.log(person.name)
+console.log(person.age)
+console.log(typeof person)
